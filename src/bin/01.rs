dfa1f7e9e0c2648db8ee3efc77868b830c4649b8
afresquet <29437693+afresquet@users.noreply.github.com>
2025-12-01 20:07:13 +0100

    day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,33 +3,53 @@ use std::str::FromStr;
 advent_of_code::solution!(1);
 
 #[derive(Debug)]
-struct Dial(u8);
+struct Dial {
+    value: i16,
+    password: u64,
+    password_new_method: u64,
+}
 
 impl Dial {
     pub fn new() -> Self {
-        Self(50)
+        Self {
+            value: 50,
+            password: 0,
+            password_new_method: 0,
+        }
     }
 
-    pub fn rotate(&mut self, rotation: &Rotation) -> u8 {
-        match rotation {
-            Rotation::Left(amount) => {
-                let amount = (amount % 100) as u8;
-                match self.0.checked_sub(amount) {
-                    Some(amount) => self.0 = amount,
-                    None => self.0 = 100 - (amount - self.0),
-                }
-            }
-            Rotation::Right(amount) => self.0 = ((self.0 as u16 + amount) % 100) as u8,
+    pub fn rotate(&mut self, rotation: &Rotation) {
+        let was_zero = self.value == 0;
+
+        self.value += rotation.value();
+
+        self.password_new_method += (self.value / 100).unsigned_abs() as u64;
+
+        if !was_zero && self.value <= 0 {
+            self.password_new_method += 1;
         }
 
-        self.0
+        self.value = self.value.rem_euclid(100);
+
+        if self.value == 0 {
+            self.password += 1;
+        }
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 enum Rotation {
-    Left(u16),
-    Right(u16),
+    Left(i16),
+    Right(i16),
+}
+
+impl Rotation {
+    pub fn value(&self) -> i16 {
+        match self {
+            Rotation::Left(amount) => -amount,
+            Rotation::Right(amount) => *amount,
+        }
+    }
 }
 
 impl FromStr for Rotation {
@@ -46,17 +66,23 @@ impl FromStr for Rotation {
     }
 }
 
-pub fn part_one(input: &str) -> Option<u64> {
+fn run(input: &str) -> Dial {
     let mut dial = Dial::new();
     let rotations = input.lines().map(|line| line.parse::<Rotation>().unwrap());
-    let password = rotations
-        .filter(|rotation| dial.rotate(rotation) == 0)
-        .count() as u64;
-    Some(password)
+    for rotation in rotations {
+        dial.rotate(&rotation)
+    }
+    dial
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let dial = run(input);
+    Some(dial.password)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let dial = run(input);
+    Some(dial.password_new_method)
 }
 
 #[cfg(test)]
@@ -72,22 +98,52 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 
     #[test]
     fn dial() {
         let mut dial = Dial::new();
-        assert_eq!(dial.rotate(&"L68".parse().unwrap()), 82);
-        assert_eq!(dial.rotate(&"L30".parse().unwrap()), 52);
-        assert_eq!(dial.rotate(&"R48".parse().unwrap()), 0);
-        assert_eq!(dial.rotate(&"L5".parse().unwrap()), 95);
-        assert_eq!(dial.rotate(&"R60".parse().unwrap()), 55);
-        assert_eq!(dial.rotate(&"L55".parse().unwrap()), 0);
-        assert_eq!(dial.rotate(&"L1".parse().unwrap()), 99);
-        assert_eq!(dial.rotate(&"L99".parse().unwrap()), 0);
-        assert_eq!(dial.rotate(&"R14".parse().unwrap()), 14);
-        assert_eq!(dial.rotate(&"L82".parse().unwrap()), 32);
+        dial.rotate(&Rotation::Left(68));
+        assert_eq!(dial.value, 82);
+        assert_eq!(dial.password, 0);
+        assert_eq!(dial.password_new_method, 1);
+        dial.rotate(&Rotation::Left(30));
+        assert_eq!(dial.value, 52);
+        assert_eq!(dial.password, 0);
+        assert_eq!(dial.password_new_method, 1);
+        dial.rotate(&Rotation::Right(48));
+        assert_eq!(dial.value, 0);
+        assert_eq!(dial.password, 1);
+        assert_eq!(dial.password_new_method, 2);
+        dial.rotate(&Rotation::Left(5));
+        assert_eq!(dial.value, 95);
+        assert_eq!(dial.password, 1);
+        assert_eq!(dial.password_new_method, 2);
+        dial.rotate(&Rotation::Right(60));
+        assert_eq!(dial.value, 55);
+        assert_eq!(dial.password, 1);
+        assert_eq!(dial.password_new_method, 3);
+        dial.rotate(&Rotation::Left(55));
+        assert_eq!(dial.value, 0);
+        assert_eq!(dial.password, 2);
+        assert_eq!(dial.password_new_method, 4);
+        dial.rotate(&Rotation::Left(1));
+        assert_eq!(dial.value, 99);
+        assert_eq!(dial.password, 2);
+        assert_eq!(dial.password_new_method, 4);
+        dial.rotate(&Rotation::Left(99));
+        assert_eq!(dial.value, 0);
+        assert_eq!(dial.password, 3);
+        assert_eq!(dial.password_new_method, 5);
+        dial.rotate(&Rotation::Right(14));
+        assert_eq!(dial.value, 14);
+        assert_eq!(dial.password, 3);
+        assert_eq!(dial.password_new_method, 5);
+        dial.rotate(&Rotation::Left(82));
+        assert_eq!(dial.value, 32);
+        assert_eq!(dial.password, 3);
+        assert_eq!(dial.password_new_method, 6);
     }
 
     #[test]
